d16d36e9933451255fdf173d6ef969f714f7b494
duwi haryanto <haryanto@uii.ac.id>
2022-05-31 10:18:10 +0700

    penambahan default route https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,8 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
-        \URL::forceScheme('https');
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
 
     }
 }
